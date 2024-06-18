b580995e7b01931b02d4abf77b2c2854a3381ed3
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-06-18 17:27:22 +0600

    2

--- app/getgenie/page.tsx
+++ app/getgenie/page.tsx
@@ -1,9 +1,8 @@
-"use client"
+"use client";
 
 import React from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { Button } from "@/components/ui/moving-border";
 
 export default function GetGenie() {
   const router = useRouter();
