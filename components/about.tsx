18f66ac4992be55c588c25eb480dc99e380cb159
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-06-22 02:07:40 +0600

    about id fix

--- components/about.tsx
+++ components/about.tsx
@@ -31,7 +31,7 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      id="About"
+      id="about"
       className="items-center max-w-[50rem] scroll-smooth"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
