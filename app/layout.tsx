23a5ecf20a73dbcf2b762fbb483b040a111ebb75
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-06-22 02:03:39 +0600

    no more dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/app/context/active-section-context";
 import Footer from "@/components/footer";
-import ThemeContextProvider from "@/app/context/theme-context";
+
 import { Toaster } from "react-hot-toast";
 import BackToTopButton from "@/components/buttontop";
 
@@ -28,16 +28,14 @@ export default function RootLayout({
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
-        <ThemeContextProvider>
-          <ActiveSectionContextProvider>
-            {children}
-            <BackToTopButton />
-            <Footer />
+        <ActiveSectionContextProvider>
+          {children}
+          <BackToTopButton />
+          <Footer />
 
-            <Toaster position="top-right" />
-            {/* <ThemeSwitch /> */}
-          </ActiveSectionContextProvider>
-        </ThemeContextProvider>
+          <Toaster position="top-right" />
+          {/* <ThemeSwitch /> */}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
--- components/experience.tsx
+++ components/experience.tsx
@@ -11,12 +11,8 @@ import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 
-import { useTheme } from "@/app/context/theme-context";
-
-
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
-  const { theme } = useTheme();
 
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
@@ -27,24 +23,15 @@ export default function Experience() {
             <VerticalTimelineElement
               visible={true}
               contentStyle={{
-                background:
-                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
                 padding: "1.3rem 2rem",
               }}
-              contentArrowStyle={{
-                borderRight:
-                  theme === "light"
-                    ? "0.4rem solid #9ca3af"
-                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
-              }}
+              contentArrowStyle={{}}
               date={item.date}
               icon={item.icon}
               iconStyle={{
-                background:
-                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                 fontSize: "1.5rem",
               }}
             >
@@ -53,7 +40,6 @@ export default function Experience() {
               <p className="!mt-1 !font-normal text-gray-700 dark:text-gray-300">
                 {item.description}
               </p>
-              
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
