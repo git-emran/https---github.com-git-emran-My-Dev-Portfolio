d2240437afcfd22a0cea7bb03ceca75e207e9229
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-06-22 01:50:21 +0600

    text fix

--- components/project.tsx
+++ components/project.tsx
@@ -42,7 +42,7 @@ export default function Project({
             <ul className="flex flex-wrap mt-4 mb-7 gap-2 sm:mt-auto">
               {tags.map((tag, index) => (
                 <li
-                  className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                  className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-black/70"
                   key={index}
                 >
                   {tag}
