1993479ab0899ff3b21096a10a8f1e23bae2d33b
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-06-22 00:23:01 +0600

    minor testimonial copy text fix

--- components/about.tsx
+++ components/about.tsx
@@ -11,13 +11,13 @@ export default function About() {
   const cardData = [
     {
       quote:
-        "This is MarketTime is a fast-paced company with ever changing priorities, and Emran did an excellent job of juggling priorities that were provided to him by multiple Agile teams. He's a great leader and I loved working with him.",
+        "MarketTime is a fast-paced company with ever changing priorities, and Emran did an excellent job of juggling priorities that were provided to him by multiple Agile teams. He's a great leader and I loved working with him.",
       name: "Todd",
       title: "President MarketTime B2B, Texas, USA",
     },
     {
       quote:
-        "Working with Emran at MarketTime was truly a pleasure. Emran displayed the ability to ask questions that not many others could. Solving customers pain points and making the best experience for them is truly Emrans passion and you can see that in his work output and in discussions with customers. I wouldn not hesitate to work with Emran again as he is truly an exceptional UI/UX Designer.",
+        "Working with Emran at was truly a pleasure. Emran displayed the ability to ask questions that not many others could. Solving customers pain points and making the best experience for them is truly Emrans passion and you can see that in his work output and in discussions with customers. I wouldn not hesitate to work with Emran again as he is truly an exceptional UI/UX Designer.",
       name: "Richard Wohnsiedler",
       title: "Sr Product Owner, ISN, Texas, USA",
     },
