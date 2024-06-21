13c8eeaddba910af70783af1dbafbf24a4cca756
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-06-22 00:12:01 +0600

    moving card fix done

--- components/about.tsx
+++ components/about.tsx
@@ -44,7 +44,7 @@ export default function About() {
         <span className="font-bold"> design problems</span> around me, creating
         illustrations, design interactions and product design memes.
       </p>
-      {/* <InfiniteMovingCards className="" items={cardData} speed="slow" /> */}
+      <InfiniteMovingCards className="" items={cardData} speed="normal" />
     </motion.section>
   );
 }
--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -96,10 +96,9 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-200 px-8 py-6 md:w-[450px]"
             style={{
-              background:
-                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+              background: "#EDEDEDDE",
             }}
             key={item.name}
           >
@@ -108,15 +107,15 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+              <span className=" relative z-20 text-sm leading-[1.6] text-gray-700 font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-sm leading-[1.6] text-gray-700 font-normal">
                     {item.name}
                   </span>
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-sm font-semibold leading-[1.6] text-gray-700">
                     {item.title}
                   </span>
                 </span>
