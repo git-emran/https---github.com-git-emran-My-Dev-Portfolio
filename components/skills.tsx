a8cff1508e5fdd3f8fd4a830a7543a15a074c0b4
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-06-25 22:32:53 +0600

    vortex added on skills section

--- components/skills.tsx
+++ components/skills.tsx
@@ -5,6 +5,7 @@ import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import { Vortex } from "./ui/vortex";
 
 const fadeInAnimationVariants = {
   initial: {
@@ -30,23 +31,25 @@ export default function Skills() {
       className="mb-28 max-w-[60rem] scroll-mt-28 scroll-mb-28 text-center sm:mb-40"
     >
       <SectionHeading>My skills</SectionHeading>
-      <ul className="flex flex-wrap justify-center gap-2 mt-12 text-lg text-blue-950-800">
-        {skillsData.map((skill, index) => (
-          <motion.li
-            className="bg-gray-100 border border-gray-200-100 rounded-lg px-7 py-3"
-            key={index}
-            variants={fadeInAnimationVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: true,
-            }}
-            custom={index}
-          >
-            {skill}
-          </motion.li>
-        ))}
-      </ul>
+      <Vortex baseSpeed={0.0} baseHue={900}>
+        <ul className="flex flex-wrap justify-center gap-2 mt-12 text-lg text-blue-950-800">
+          {skillsData.map((skill, index) => (
+            <motion.li
+              className="bg-gray-100 border border-gray-200-100 rounded-lg px-7 py-3"
+              key={index}
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{
+                once: true,
+              }}
+              custom={index}
+            >
+              {skill}
+            </motion.li>
+          ))}
+        </ul>
+      </Vortex>
     </section>
   );
 }
