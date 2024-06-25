db9e2e7067f4c3818f82c958a7e7249b12d4aa05
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-06-25 22:59:43 +0600

    faq data update

--- components/faq.tsx
+++ components/faq.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import {
   Accordion,
@@ -11,7 +11,10 @@ import { LinkPreview } from "./ui/link-preview";
 
 export default function Faq() {
   return (
-    <div id="faq" className="w-full mb-28 max-w-[60rem] scroll-mt-20 scroll-mb-28 text-center sm:mb-40 sm:p-30">
+    <div
+      id="faq"
+      className="w-full mb-28 max-w-[60rem] scroll-mt-20 scroll-mb-28 text-center sm:mb-40 sm:p-30"
+    >
       <SectionHeading>Frequently Asked Questions & Answers</SectionHeading>
       <Accordion type="single" collapsible className="w-full mt-12">
         <AccordionItem value="item-1">
@@ -19,7 +22,9 @@ export default function Faq() {
           <AccordionContent>
             My core strengths as a Product designer includes my ability to find
             clarity through ambiguity, navigate and scale any idea even if its
-            just a single sentence. Also I can code myself, which makes me a team player who understand both the complexity and feasability of the designs that I create.
+            just a single sentence. Also I can code myself, which makes me a
+            team player who understand both the complexity and feasability of
+            the designs that I create.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
@@ -56,7 +61,9 @@ export default function Faq() {
             Did you build this site by yourself ?
           </AccordionTrigger>
           <AccordionContent>
-      Yes I hard coded this site by myself. Using react, tailwind css, nextjs, framer-motion and typescript. I also know swift & swiftUI.  
+            Yes I hard coded this site by myself. Using react, tailwind css,
+            nextjs, framer-motion and typescript, Hosting through Vercel. I also
+            personally love building apps using swift & swiftUI.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
