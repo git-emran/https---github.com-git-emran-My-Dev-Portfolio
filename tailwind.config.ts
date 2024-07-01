5e7c222a4b93deecc2c03112b827accfa9deaeb2
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-07-01 22:01:54 +0600

    another try at bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,6 +94,10 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        noise:
+          "/public/noise.png')",
+      },
     },
   },
   plugins: [addVariablesForColors, require("tailwindcss-animate")],
