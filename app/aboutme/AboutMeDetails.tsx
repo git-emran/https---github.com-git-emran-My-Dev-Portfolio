b0d05ef9a8814079cd143e1985e446a188e4565d
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-07-14 08:37:52 +0600

    About me details section broiler plate added

---
+++ app/aboutme/AboutMeDetails.tsx
@@ -0,0 +1,10 @@
+import SubpageHeader from '@/components/subpage-header';
+import React from 'react'
+
+function AboutMeDetails() {
+  return <div className="min-h-screen sm:top-20 flex flex-col">
+    <SubpageHeader title='Learn More About Me' subtitle='How it all got started' />
+  </div>;
+}
+
+export default AboutMeDetails
