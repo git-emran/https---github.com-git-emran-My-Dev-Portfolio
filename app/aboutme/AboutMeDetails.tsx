cd6ea0311e81d7fbd03754bd4f34091f5174057e
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-07-16 13:54:43 +0700

    minor fix

--- app/aboutme/AboutMeDetails.tsx
+++ app/aboutme/AboutMeDetails.tsx
@@ -2,14 +2,18 @@ import SubpageHeader from '@/components/subpage-header';
 import React from 'react'
 
 function AboutMeDetails() {
-  return <div className="min-h-screen sm:top-20 flex flex-col">
-    <SubpageHeader title='A Glimpse into the Years' subtitle='How it all got started' />
-    {/* Start of the Body   */}
-    <div>
-        
+  return (
+    <div className="min-h-screen sm:top-20 flex flex-col">
+      <SubpageHeader
+        title="A Glimpse into the Years"
+        subtitle="How it all got started"
+      />
+      {/* Start of the Body   */}
+      <section className='p-8 bg-gray-100"'>
+        <div className="max-w-4xl mx-auto"></div>
+      </section>
     </div>
-
-  </div>;
+  );
 }
 
 export default AboutMeDetails
