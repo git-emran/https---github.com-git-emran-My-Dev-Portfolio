1bbb23cdd5b1314b00026912f90804254bbba886
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-03 01:05:37 +0600

    text fix minor

--- app/otherprojects/page.tsx
+++ app/otherprojects/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import SubpageHeader from "@/components/subpage-header";
 import { ExpandableCardDemo } from "@/components/ui/expandableCard";
@@ -8,18 +8,15 @@ import React from "react";
 function OtherProjects() {
   return (
     <div className="min-h-screen sm:top-20 flex flex-col">
-    
       {/* Start of the Body   */}
-     <section>
-      <h1 className="font-bold text-4xl flex items-center justify-center mt-20">
-        Other Fun Projects That I worked On
-      </h1>
-     </section>
-      <section className="mt-20 flex">
-<ExpandableCardDemo />
+      <section>
+        <h1 className="font-bold text-4xl flex items-center justify-center mt-20">
+          Other Fun Projects
+        </h1>
       </section>
-      
-  
+      <section className="mt-20 flex">
+        <ExpandableCardDemo />
+      </section>
     </div>
   );
 }
