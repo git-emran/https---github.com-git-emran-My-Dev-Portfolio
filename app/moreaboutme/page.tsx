6f71e2fa8bb0a115dae7af0ca72c531298d4660a
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-04 23:30:12 +0600

    css update

--- app/moreaboutme/page.tsx
+++ app/moreaboutme/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 export default function MoreAboutMe() {
   return (
     <div>
-      <section>
+      <section className="min-h-screen sm:top-20 flex flex-col">
         <h1 className="font-bold text-3xl">More About me</h1>
       </section>
+      <section></section>
     </div>
   );
 }
