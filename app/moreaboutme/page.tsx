12c1a16e3a2fe2b97a13f28e546651b1dc1bcea8
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-04 23:27:51 +0600

    text size update

--- app/moreaboutme/page.tsx
+++ app/moreaboutme/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MoreAboutMe() {
   return (
     <div>
-        <h1 className='font-bold'>
+        <h1 className='font-bold text-3xl'>
             More About me
         </h1>
     </div>
