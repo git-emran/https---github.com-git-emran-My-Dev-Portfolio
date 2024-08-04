ef73c4609274d46a846fc8fd9458b2dca8d43e15
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-04 23:27:05 +0600

    text update 2 : mam

--- app/moreaboutme/page.tsx
+++ app/moreaboutme/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MoreAboutMe() {
   return (
     <div>
-        <h1>
+        <h1 className='font-bold'>
             More About me
         </h1>
     </div>
