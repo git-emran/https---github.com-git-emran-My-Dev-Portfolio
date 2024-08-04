013d42ce8a91d7146e316ec6017c6f8bba790a75
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-04 23:28:39 +0600

    section integrated

--- app/moreaboutme/page.tsx
+++ app/moreaboutme/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function MoreAboutMe() {
   return (
     <div>
-        <h1 className='font-bold text-3xl'>
-            More About me
-        </h1>
+      <section>
+        <h1 className="font-bold text-3xl">More About me</h1>
+      </section>
     </div>
-  )
+  );
 }
