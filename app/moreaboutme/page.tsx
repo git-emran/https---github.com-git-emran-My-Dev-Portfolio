62bab428dcaa6f7c44700a99a9aaaabc1a051d61
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-07 02:05:47 +0600

    New Section introduced

--- app/moreaboutme/page.tsx
+++ app/moreaboutme/page.tsx
@@ -1,17 +1,11 @@
+"use client";
+import { FeaturesSectionDemo } from "@/components/FeaturedAbout";
 import React from "react";
 
 export default function MoreAboutMe() {
   return (
-    <div>
-      <section className="min-h-screen sm:top-20 flex flex-col">
-        <h1 className="font-bold text-3xl">More About me</h1>
-      </section>
-      <section>
-        <p className="text-sm">
-          I have always been drawn to the <span className="italic font-medium"> creative</span>
-          {""}side of Things. Here is how it all started!!
-        </p>
-      </section>
+    <div className="">
+      <FeaturesSectionDemo />
     </div>
   );
 }
