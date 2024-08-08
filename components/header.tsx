362446fe30ee0de1565038bf344d8ce0249aa04f
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-08 22:39:32 +0600

    minor css updates

--- components/header.tsx
+++ components/header.tsx
@@ -12,8 +12,8 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-75 shadow-lg shadow-black/[0.03] backdrop-blur-[12rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full"
-        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-75 shadow-lg shadow-black/[0.03] backdrop-blur-[2rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full "
+        initial={{ y: -100, x: "-50%", opacity: 1 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
