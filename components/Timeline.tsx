e14cec95cd46b6eca8a45b9e814ee83586a4ba76
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-15 21:35:12 +0600

    Final timeline fixes added

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,16 +1,13 @@
-"use client";
-
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { StringToBoolean } from "class-variance-authority/types";
 
 interface TimelineItemProps {
   date: string;
   title: string;
   description: string;
   imageUrl: string;
-  status: String; // You can extend this for other statuses
+  status: string;
 }
 
 const TimelineItem: React.FC<TimelineItemProps> = ({
@@ -22,29 +19,30 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
 }) => {
   return (
     <motion.div
-      className="flex items-start space-x-4 p-4 border-l-4 border-transparent hover:border-l-4 hover:border-blue-500 transition-all duration-300"
+      className="flex items-start space-x-4 p-4"
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true }}
+      viewport={{ once: true, amount: 0.2 }}
       transition={{ duration: 0.5 }}
     >
-      <div className="flex flex-col items-center">
+      <div className="relative flex">
         <span
           className={`block h-4 w-4 rounded-full ${
             status === "update" ? "bg-red-500" : "bg-purple-500"
           }`}
         ></span>
-        <div className="h-full w-1 bg-gray-200"></div>
+        {/* Timeline bar */}
+        <div className="absolute top-5 left-1/2 transform -translate-x-1/2 w-0.5 bg-gray-400 h-[20rem] opacity-40 items-center justify-center"></div>
       </div>
-      <div className="ml-4">
+      <div className="">
         <p className="text-sm text-gray-500">{date}</p>
         <h3 className="text-xl font-semibold">{title}</h3>
         <p className="text-gray-700">{description}</p>
         <Image
           src={imageUrl}
           alt={title}
-          width={900}
           height={700}
+          width={900}
           className="mt-4 rounded-lg shadow-lg object-cover w-64 h-40"
         />
       </div>
