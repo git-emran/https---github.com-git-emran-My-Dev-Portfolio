b5bc61b15beab48678c59e5a222a7a05b0db7175
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-03-17 23:44:39 +0600

    cv and timeline details updated

--- app/mytimeline/page.tsx
+++ app/mytimeline/page.tsx
@@ -14,50 +14,45 @@ import BackToHome from "@/components/back-to-home";
 
 export default function MyTimeline() {
   const { ref } = useSectionInView("Experience", 0.5);
+
   return (
-    <section
-      id="experience"
-      ref={ref}
-      className="scroll-mt-28 mt-6 pb-10 sm:mb-30 w-full"
-    >
+    <section ref={ref} className="scroll-mt-28 mt-6 pb-10 sm:mb-30 w-full">
       <BackToHome />
       <BackToTopButton />
       <div className="text-center">
         <DescriptionSection
           title="Full Job Experience"
-          description="With over 8 years of experience, I have managed teams and also tackled situations Solo"
+          description="With over 8 years of experience, I have managed teams and tackled many bottlenecks."
         />
       </div>
-      <div className="">
-        {" "}
-        <VerticalTimeline lineColor="">
+      <div>
+        <VerticalTimeline lineColor="#ededed">
+          {" "}
+          {/* Added a default line color */}
           {experiencesData.map((item, index) => (
-            <React.Fragment key={index}>
-              <VerticalTimelineElement
-                visible={true}
-                contentStyle={{
-                  border: "1px solid rgba(0, 0, 0, 0.05)",
-                  textAlign: "left",
-                  padding: "1.3rem 2rem",
-                }}
-                contentArrowStyle={{}}
-                date={item.date}
-                icon={item.icon}
-                iconStyle={{
-                  background: "rgb(32, 99, 233)",
-                  color: "#fff",
-                  fontSize: "1.5rem",
-                }}
-              >
-                <h3 className="font-bold text-black capitalize">
-                  {item.title}
-                </h3>
-                <p className="font-normal pb-3 !mt-0">{item.location}</p>
-                <p className="!mt-1 !font-normal text-gray-700">
-                  {item.description}
-                </p>
-              </VerticalTimelineElement>
-            </React.Fragment>
+            <VerticalTimelineElement
+              key={index}
+              visible={true}
+              contentStyle={{
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{}}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background: "rgb(32, 99, 233)",
+                color: "#fff",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3 className="font-bold text-black capitalize">{item.title}</h3>
+              <p className="font-normal pb-3 !mt-0">{item.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700">
+                {item.description}
+              </p>
+            </VerticalTimelineElement>
           ))}
         </VerticalTimeline>
       </div>
