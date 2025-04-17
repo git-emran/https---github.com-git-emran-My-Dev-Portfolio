71922f87cd5882ee917606094cd34cbcbc4ef832
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-04-17 19:43:03 +0600

    More about me section added

---
+++ app/moreaboutme/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import SubpageHeader from "@/components/subpage-header";
+import DescriptionSection from "@/components/DescriptionSection";
+import BackToTopButton from "@/components/buttontop";
+
+export default function MoreAbout() {
+  return (
+    <main>
+      {/* header section */}
+      <BackToTopButton />
+      <SubpageHeader
+        title="About me"
+        subtitle="Learn more about me, what I like to do and my work"
+      />
+      {/* Case study starts here */}
+
+      {/* Background */}
+  
+    </main>
+  );
+}
