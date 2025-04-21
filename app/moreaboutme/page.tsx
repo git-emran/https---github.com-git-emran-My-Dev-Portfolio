c0e6cc678c43cee998b0dbdd8ffcd56e114eabc4
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-04-21 18:01:24 +0600

    about me section background added

--- app/moreaboutme/page.tsx
+++ app/moreaboutme/page.tsx
@@ -13,10 +13,13 @@ export default function MoreAbout() {
         subtitle="Learn more about me, what I like to do and my work"
       />
       {/* Case study starts here */}
-      <DescriptionSection title="About me" description="A little bit of this, a little bit of that" />
+      <DescriptionSection
+        title="About me"
+        description="A little bit of this, a little bit of that"
+      />
 
       {/* Background */}
-  
+      <section className="p-8 bg-gray-100"></section>
     </main>
   );
 }
