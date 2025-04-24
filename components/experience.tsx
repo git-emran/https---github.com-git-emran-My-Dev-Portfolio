a7138a93bd11cef6b0f75f64fc40b70b99a59247
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-04-25 00:03:54 +0600

    Experiences button unified

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,6 +5,8 @@ import { useSectionInView } from "@/lib/hooks";
 import DescriptionSection from "./DescriptionSection";
 import { Timeline } from "@/components/ui/timeline";
 import Link from "next/link";
+import { Button } from "@radix-ui/themes";
+import { MdOutlineArrowOutward } from "react-icons/md";
 
 const experiencesdata = [
   {
@@ -105,15 +107,20 @@ export default function Experience() {
       className="scroll-mt-28 mt-6 pb-10 sm:mb-30 scroll-smooth bg-gray-100 w-full"
     >
       <div className="text-center">
-        <DescriptionSection title="Experience" description="Over 9+ years of professional working experience" badge="Featured Positions" />
+        <DescriptionSection
+          title="Experience"
+          description="Over 9+ years of professional working experience"
+          badge="Featured Positions"
+        />
       </div>
       <div className="items-center justify-center text-start">
         <Timeline data={experiencesdata} />
       </div>
       <div className="items-center justify-center flex ">
-        <button className=" bg-white cursor-pointer border font-medium border-gray-200 hover:bg-slate-200 text-sm px-10 py-4 rounded-md mt-14">
-          <Link href="/mytimeline"> View Full Timeline </Link>
-        </button>
+        <Button className="hover:bg-blue-800 cursor-pointer">
+            <MdOutlineArrowOutward />
+            <Link href="/mytimeline"> View Full Experience </Link>
+          </Button>
       </div>
     </section>
   );
