7466e165b4bdd2e2fa5eeabea3ca471f393baf80
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-05-01 20:37:58 +0600

    color update website

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mb-10 mt-40 px-4 text-center text-gray-500">
-      <small className="mb-2 pt-100 mt-100 block text-xs">
+    <footer className="pb-10 mt-40 px-4 text-center text-gray-500">
+      <small className="mb-2 pt-50 mt-50 block text-xs">
         &copy; 2030 Emran. All rights reserved.
       </small>
       <p className="text-xs">
